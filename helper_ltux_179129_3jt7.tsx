32735dc6cbb1dc2b1d2d7af76669d67b8fec49b5
SvetaBTC <fevralovasvitlana@gmail.com>
2024-03-17 04:48:34 +0200

    Refactored configurations in performance

---
+++ helper_ltux_179129_3jt7.tsx
@@ -0,0 +1,3 @@
+const getRandomElement = array => array[getRandomIndex(array)];
+const curry = (fn, arity = fn.length, ...args) => arity <= args.length ? fn(...args) : curry.bind(null, fn, arity, ...args);
+
